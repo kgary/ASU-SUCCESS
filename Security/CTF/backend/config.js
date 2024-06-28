0f89a703bc204342f308aa30df4830d94ff40571
Karthik Vaida <jvaida@asu.edu>
2024-06-27 20:34:42 -0700

    Mongodb cloud URL re-added

--- Security/CTF/backend/config.js
+++ Security/CTF/backend/config.js
@@ -1,3 +1,5 @@
 export const PORT = 5555;
-export const mongoDBURL = `mongodb://192.168.0.40:27017/success2024`;
+const password = "#SUCCESS2024";
+const encodedPassword = encodeURIComponent(password);
+export const mongoDBURL = `mongodb+srv://vjk2018:${encodedPassword}@cluster0.hbpm1y6.mongodb.net/`;
 export const CIPHER_CODE = "A5U"
