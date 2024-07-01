fde60145d944a052776e65a9a5712a7962e18578
kgary <kgary@asu.edu>
2024-07-01 12:35:56 -0700

    Added constants and some challenges to the first Basic Arduino script

--- SysAndNetworking/LEDMatrix/Basic/Basic.ino
+++ SysAndNetworking/LEDMatrix/Basic/Basic.ino
@@ -10,28 +10,42 @@ void setup() {
 }
 
 void loop() {
-  uint8_t pixels[64];
+
+ // The dimensions of the array
+ const int MATRIX_ROWS = 8;
+ const int MATRIX_COLS = 12;
+ const int DELAY = 500;   // This is how long to wait before redrawing the next thing
+
+ // This represents the LED Matrix
+ uint8_t pixels[MATRIX_ROWS * MATRIX_COLS];
 
  // Turn on all LEDs
-  for (int row = 0; row < 8; row++) {
-    for (int col = 0; col < 12; col++) {
-      pixels[row * 12 + col] = 1; // Turn on the pixel
+  for (int row = 0; row < MATRIX_ROWS; row++) {
+    for (int col = 0; col < MATRIX_COLS; col++) {
+      pixels[row * MATRIX_COLS + col] = 1; // Turn on the pixel
     }
   }
-  matrix.loadPixels(pixels, 96); // Update the display
-  delay(500);    // Wait for half a second
+  matrix.loadPixels(pixels, MATRIX_ROWS * MATRIX_COLS); // Update the display
+  delay(DELAY);    // Wait for DELAY
 
   // Turn off all LEDs
-  for (int row = 0; row < 8; row++) {
-    for (int col = 0; col < 12; col++) {
-      pixels[row * 12 + col] = 0; // Turn off the pixel
+  for (int row = 0; row < MATRIX_ROWS; row++) {
+    for (int col = 0; MATRIX_COLS < 12; col++) {
+      pixels[row * MATRIX_COLS + col] = 0; // Turn off the pixel
     }
   }
-  matrix.loadPixels(pixels, 96); // Update the display
-  delay(500);    // Wait for half a second
+  matrix.loadPixels(pixels, MATRIX_ROWS * MATRIX_COLS); // Update the display
+  delay(DELAY);    // Wait for DELAY
+
+  // TRY THIS
+  // 1. Can you write loops to make the middle row only light up?
+  // 2. Can you write loops to make the middle column light up?
+  // 3. Can you combine 1 and 2 and make a + sign?
+  // 4. How do you make the diagonal light up?
+  // 5. (advanced) Can you make a line move across the matrix?
 
   // Define frames
-uint8_t happy[] = {
+  uint8_t happy[] = {
     0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
     0, 0, 1, 1, 0, 0, 0, 0, 1, 1, 0, 0,
     0, 0, 1, 1, 0, 0, 0, 0, 1, 1, 0, 0,
@@ -54,10 +68,15 @@ uint8_t happy[] = {
   };
 
   // Display happy face
-  matrix.loadPixels(happy, 96);
-  delay(500);
+  matrix.loadPixels(happy, MATRIX_ROWS * MATRIX_COLS);
+  delay(DELAY); // Wait for DELAY
 
   // Display heart
-  matrix.loadPixels(heart, 96);
-  delay(500);
+  matrix.loadPixels(heart, MATRIX_ROWS * MATRIX_COLS);
+  delay(DELAY); // Wait for DELAY
+
+  // TRY THIS
+  // 1. Create your own shape!
+  // 2. (advanced) Can you make your shape moved across the matrix 
+  //    without manually setting the matrix (hint: use loops like above)
 }
